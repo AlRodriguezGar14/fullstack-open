c29a068bc911985f0af6122e3ad6311eaeecdbe0
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-06-04 02:18:24 +0200

    removed non-used imports and useless comments

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,14 +1,7 @@
 import {Header} from "./Header";
 import {Content} from "./Content";
-import {Total} from "./Total";
 
 const App = () => {
-  // const-definitions
-// const courseList = [
-//   { name: "Fundamentals of React", exercises: 10, id: 1 },
-//   { name: "Using props to pass data", exercises: 7, id: 2 },
-//   { name: "State of a component", exercises: 14, id: 3 },
-// ];
 
 const courseList = [
     {

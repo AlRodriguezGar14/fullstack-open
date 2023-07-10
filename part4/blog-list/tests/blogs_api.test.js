ad7211d452d091a04e1ea5bc3b6042319fb44a41
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-07-10 15:29:29 +0200

    [4.10] Blog list tests, step 3. Test that the app posts correctly a blog. Test the blogs in the system are increased by one. Test the content of the new blog post

--- part4/blog-list/tests/blogs_api.test.js
+++ part4/blog-list/tests/blogs_api.test.js
@@ -11,18 +11,41 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-afterAll(async () => {
-  await mongoose.connection.close();
-});
-
-test("there is one blog post", async () => {
-  const response = await api.get("/api/blogs");
-
-  expect(response.body).toHaveLength(1);
-});
+// test("there is one blog post", async () => {
+//   const response = await api.get("/api/blogs");
+//
+//   expect(response.body).toHaveLength(1);
+// });
 
 test("verify there is id", async () => {
   const res = await api.get("/api/blogs");
 
   expect(res.body[0].id).toBeDefined();
+});
+
+test("post succesfully creates valid posts", async () => {
+  const initialResponse = await api.get("/api/blogs");
+
+  const newBlog = {
+    title: "testing blog",
+    author: "testing library",
+    url: "www.tojest.com",
+    likes: 0,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const newResponse = await api.get("/api/blogs");
+  expect(newResponse.body).toHaveLength(initialResponse.body.length + 1);
+  expect(newResponse.body[newResponse.body.length - 1].title).toContain(
+    "testing"
+  );
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });

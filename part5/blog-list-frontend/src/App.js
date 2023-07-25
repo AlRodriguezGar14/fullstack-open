86fe0444b6537d42b812eafe3aec38e84a6330fe
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-07-25 20:20:17 +0200

    [not done during exercises] login and blog forms created

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -6,24 +6,13 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
   const [readPassword, setReadPassword] = useState(false);
+  const [user, setUser] = useState(null);
 
-  const toggleReadPassword = (event) => {
-    event.preventDefault();
-    setReadPassword(!readPassword);
-  };
-
-  const passwordFormType = readPassword === false ? "password" : "text";
-
-  useEffect(() => {
-    blogService.getAll().then((fetched) => setBlogs(fetched));
-  }, []);
-
-  return (
-    <div>
+  const loginForm = () => (
+    <>
       <h2>login</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         <div>
           username
           <input
@@ -42,10 +31,54 @@ const App = () => {
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
+        <button type="submit">Login</button>
         <button onClick={toggleReadPassword} style={{ margin: "10px" }}>
           Show / Hide the password
         </button>
       </form>
+    </>
+  );
+
+  const blogForm = () => (
+    <>
+      <h2>Add a blog</h2>
+      <form onSubmit={handleBlogSubmit}>
+        title
+        <input type="text" value="title" />
+        author
+        <input type="text" value="author" />
+        url
+        <input type="text" value="url" />
+        likes
+        <input type="number" value="0" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+
+  const toggleReadPassword = (event) => {
+    event.preventDefault();
+    setReadPassword(!readPassword);
+  };
+
+  const handleBlogSubmit = (event) => {
+    event.preventDefault();
+    console.log("sumitting blog...");
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(`logging in with ${username} ${password}`);
+  };
+  const passwordFormType = readPassword === false ? "password" : "text";
+
+  useEffect(() => {
+    blogService.getAll().then((fetched) => setBlogs(fetched));
+  }, []);
+
+  return (
+    <div>
+      {user === null ? loginForm() : blogForm()}
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />

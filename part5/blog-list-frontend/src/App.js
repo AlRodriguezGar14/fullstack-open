f9bd2a63355e931c07845dead3c8d46eb573f45c
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-07-25 19:37:45 +0200

    [not required] Created the frontend. Added the option to toggle the password

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -4,6 +4,17 @@ import blogService from "./services/blogs";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [readPassword, setReadPassword] = useState(false);
+
+  const toggleReadPassword = (event) => {
+    event.preventDefault();
+    setReadPassword(!readPassword);
+  };
+
+  const passwordFormType = readPassword === false ? "password" : "text";
 
   useEffect(() => {
     blogService.getAll().then((fetched) => setBlogs(fetched));
@@ -11,6 +22,30 @@ const App = () => {
 
   return (
     <div>
+      <h2>login</h2>
+      <form>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type={passwordFormType}
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button onClick={toggleReadPassword} style={{ margin: "10px" }}>
+          Show / Hide the password
+        </button>
+      </form>
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,5 +1,12 @@
 const Blog = ({ blog }) => (
-  <div>
+  <div
+    style={{
+      boxShadow: "0 0 3px 2px black",
+      padding: "2px",
+      marginTop: "10px",
+      marginBottom: "10px",
+    }}
+  >
     <p>
       "{blog.title}" by {blog.author}
     </p>

b32bd6e51f865c352733d11062366952d85c7a1b
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-07-28 16:58:33 +0200

    [5.5] Blog list frontend: toggle the visibility of the login and new blog forms

--- part5/blog-list-frontend/src/App.js
+++ part5/blog-list-frontend/src/App.js
@@ -10,6 +10,20 @@ const App = () => {
   const [readPassword, setReadPassword] = useState(false);
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState(null);
+  const [loginVisible, setLoginVisible] = useState(false);
+
+  const [newBlogVisibility, setNewBlogVisibility] = useState(false);
+
+  const newBlogVisible = { display: newBlogVisibility ? "" : "none" };
+  const newBlogBtnVisible = { display: newBlogVisibility ? "none" : "" };
+
+  const loginFormVisibility = {
+    display: loginVisible ? "" : "none",
+  };
+
+  const logBtnVisiblity = {
+    display: loginVisible ? "none" : "",
+  };
 
   const [blogPost, setBlogPost] = useState({
     title: "",
@@ -18,74 +32,113 @@ const App = () => {
     likes: 0,
   });
 
+  const toggleLoginVisibility = (e) => {
+    e.preventDefault();
+    setLoginVisible(!loginVisible);
+  };
+
+  const toggleNewBlogVisibility = (e) => {
+    e.preventDefault();
+    setNewBlogVisibility(!newBlogVisibility);
+    console.log(newBlogVisibility);
+  };
+
   const loginForm = () => (
     <>
-      <h2>login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type={passwordFormType}
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-        <button onClick={toggleReadPassword} style={{ margin: "10px" }}>
-          Show / Hide the password
-        </button>
-      </form>
+      <button
+        type="submit"
+        onClick={toggleLoginVisibility}
+        style={logBtnVisiblity}
+      >
+        Log In
+      </button>
+      <div className="form" style={loginFormVisibility}>
+        <h2>login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type={passwordFormType}
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+          <button type="submit" onClick={toggleLoginVisibility}>
+            Cancel
+          </button>
+          <button
+            type="submit"
+            onClick={toggleReadPassword}
+            style={{ margin: "10px" }}
+          >
+            Show / Hide the password
+          </button>
+        </form>
+      </div>
     </>
   );
 
   const blogForm = () => (
     <>
-      <h2>Add a blog</h2>
-      <form onSubmit={handleBlogSubmit}>
-        title
-        <input
-          type="text"
-          value={blogPost["title"]}
-          onChange={(event) =>
-            setBlogPost({ ...blogPost, title: event.target.value })
-          }
-        />
-        author
-        <input
-          type="text"
-          value={blogPost["author"]}
-          onChange={(event) =>
-            setBlogPost({ ...blogPost, author: event.target.value })
-          }
-        />
-        url
-        <input
-          type="text"
-          value={blogPost["url"]}
-          onChange={(event) =>
-            setBlogPost({ ...blogPost, url: event.target.value })
-          }
-        />
-        likes
-        <input
-          type="number"
-          value={blogPost["likes"]}
-          onChange={(event) =>
-            setBlogPost({ ...blogPost, likes: event.target.value })
-          }
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <button
+        type="submit"
+        onClick={toggleNewBlogVisibility}
+        style={newBlogBtnVisible}
+      >
+        New Blog
+      </button>
+      <div className="NewBlogForm" style={newBlogVisible}>
+        <h2>Add a blog</h2>
+        <form onSubmit={handleBlogSubmit}>
+          title
+          <input
+            type="text"
+            value={blogPost["title"]}
+            onChange={(event) =>
+              setBlogPost({ ...blogPost, title: event.target.value })
+            }
+          />
+          author
+          <input
+            type="text"
+            value={blogPost["author"]}
+            onChange={(event) =>
+              setBlogPost({ ...blogPost, author: event.target.value })
+            }
+          />
+          url
+          <input
+            type="text"
+            value={blogPost["url"]}
+            onChange={(event) =>
+              setBlogPost({ ...blogPost, url: event.target.value })
+            }
+          />
+          likes
+          <input
+            type="number"
+            value={blogPost["likes"]}
+            onChange={(event) =>
+              setBlogPost({ ...blogPost, likes: event.target.value })
+            }
+          />
+          <button type="submit">Submit</button>
+          <button type="submit" onClick={toggleNewBlogVisibility}>
+            Cancel
+          </button>
+        </form>
+      </div>
     </>
   );
 
@@ -157,15 +210,18 @@ const App = () => {
         loginForm()
       ) : (
         <div>
-          <p>logged as {user.username}</p>
-          <button onClick={handleLogout}>logout</button>
+          <div>
+            <p>logged as {user.username}</p>
+            <button onClick={handleLogout}>logout</button>
+          </div>
           {blogForm()}
-          <h2>blogs</h2>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
         </div>
       )}
+
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 };

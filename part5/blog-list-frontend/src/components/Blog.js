056d2d53b7af89a2a3196bc2453766297f14177f
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-07-28 21:24:08 +0200

    [5.7 Blog list frontend. Show/Hide the additional content of the blog]

--- part5/blog-list-frontend/src/components/Blog.js
+++ part5/blog-list-frontend/src/components/Blog.js
@@ -1,16 +1,46 @@
-const Blog = ({ blog }) => (
-  <div
-    style={{
-      boxShadow: "0 0 3px 2px black",
-      padding: "2px",
-      marginTop: "10px",
-      marginBottom: "10px",
-    }}
-  >
-    <p>
-      "{blog.title}" by {blog.author}
-    </p>
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showBlog, setShowBlog] = useState(false);
+  const showHide = { display: showBlog ? "" : "none" };
+
+  return (
+    <div
+      style={{
+        boxShadow: "0 0 3px 2px black",
+        padding: "5px 15px",
+        marginBottom: "10px",
+      }}
+    >
+      <section>
+        <p>
+          "{blog.title}" by {blog.author}
+        </p>
+        <BlogShowBtn
+          key={"showBlogUniqueBtn"}
+          showBlog={showBlog}
+          setShowBlog={setShowBlog}
+          blog={blog}
+        />
+      </section>
+      <section style={showHide}>
+        <p>{blog.url}</p>
+        <p>{blog.likes}</p>
+        <p>{blog.user.username}</p>
+      </section>
+    </div>
+  );
+};
+
+const BlogShowBtn = ({ showBlog, setShowBlog, blog }) => {
+  const content = showBlog ? "hide" : "show more";
+
+  const handleBlogShow = (e) => {
+    e.preventDefault();
+    setShowBlog(!showBlog);
+  };
+
+  return <button onClick={handleBlogShow}>{content}</button>;
+};
 
 export default Blog;

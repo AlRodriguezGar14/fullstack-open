833dc3ae7657844bbbad439a33246dadc3f56b9f
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-08-16 17:02:58 +0200

    6.1: unicafe revisited, step1
    Make the tests work

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import counterReducer from "./reducer";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({ reducer: counterReducer });
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: "GOOD",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);

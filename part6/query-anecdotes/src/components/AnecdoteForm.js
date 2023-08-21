1aa19c505604dbbae221c5856063c71a3f1df1e9
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-08-21 20:47:33 +0200

    Exercise 6.21
    Implement adding new anecdotes to the server using React Query. The application should render a new anecdote by default.

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,21 +1,34 @@
+import { useMutation, useQueryClient } from "react-query";
+import { createAnecdote } from "../services/requests";
+
 const AnecdoteForm = () => {
+  const newAnecdoteMutation = useMutation(createAnecdote);
+  const queryClient = useQueryClient();
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    console.log("new anecdote");
+    newAnecdoteMutation.mutate(
+      { content: content, votes: 0 },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries("anecdotes");
+        },
+      }
+    );
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
--- part6/query-anecdotes/src/services/requests.js
+++ part6/query-anecdotes/src/services/requests.js
@@ -3,3 +3,6 @@ import axios from "axios";
 const baseURl = "http://localhost:3001/anecdotes";
 
 export const getAnecdotes = () => axios.get(baseURl).then((res) => res.data);
+
+export const createAnecdote = (newAnecdote) =>
+  axios.post(baseURl, newAnecdote).then((res) => res.data);
